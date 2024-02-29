16d9fbb5bbf70218cac48fb7a39459c8aed1e379
Eunchan Lee <eunchan.lee@kaist.ac.kr>
2024-02-29 23:22:19 +0900

    feature: petsc try on intermediate velocity with TDMA

--- petsc/try/app.c
+++ petsc/try/app.c
@@ -1,5 +1,9 @@
+#include <stdlib.h>
 #include <petscdmda.h>
 #include <petscksp.h>
+#include <petscsys.h>
+#include <petscvec.h>
+#include "../../include/config.h"
 
 #define DT 0.001
 #define NX 32
@@ -10,25 +14,29 @@
 #define U_WALL 1.0
 
 PetscReal x[NX], y[NY], xc[NX], yc[NY];
-double conxold[NX][NY], conyold[NX][NY];
+double conxold[NY][NX], conyold[NY][NX];
+PC pc;
+
+void printMatrix(double mtx[NY][NX]);
 
 // Function declarations
-PetscErrorCode updateIntermediateVelocity_fs(DM da, Vec u_vec, Vec v_vec,
+void updateIntermediateVelocity_fs(DM da, Vec u_vec, Vec v_vec,
                                              Vec p_vec, Vec u_hat_vec,
                                              Vec v_hat_vec);
-PetscErrorCode updatePressureField(DM da, Vec u_vec, Vec v_vec, Vec p_vec);
+void updatePressureField(DM da, Vec u_vec, Vec v_vec, Vec p_vec);
 void updateVelocityField(DM da, Vec u_vec, Vec v_vec, Vec p_vec, Vec u_hat_vec,
                          Vec v_hat_vec);
 
-void thomasMethod2D(int n, double a[], double b[], double c[], double d[][n],
-                    double x[][n]);
-void getLinearSolutionWithThomas(int dim, int numOfSolutions,
-                                 double A[dim][dim],
-                                 double RHS[][numOfSolutions],
-                                 double x[][numOfSolutions]);
 void setPressureBoundaryConditions(DM da, Vec p_vec);
 void setVelocityBoundaryConditions(DM da, Vec u_vec, Vec v_vec);
 void setBoundaryConditions(DM da, Vec u_vec, Vec v_vec, Vec p_vec);
+
+PetscErrorCode petsc_matrixvector_init(Mat *A, Vec *x, Vec *b, DM da,
+                              double A1_fs[][NX], double A2_fs[][NX]);
+PetscErrorCode petsc_ksp_init(KSP *ksp, Mat A);
+PetscErrorCode petsc_solve(double solution[][NX], DM da, Vec *x, Vec *b, KSP ksp, double rhs[][NX]);
+
+void petsc_dmda_init(DM *da);
 
 int main(int argc, char **argv) {
   PetscInt Nx = NX, Ny = NY;
@@ -53,13 +61,7 @@ int main(int argc, char **argv) {
 
   // Create DMDA
   DM da;
-  DMDACreate2d(PETSC_COMM_WORLD, DM_BOUNDARY_NONE, DM_BOUNDARY_NONE,
-               DMDA_STENCIL_STAR, Nx, Ny, PETSC_DECIDE, PETSC_DECIDE, 1, 1,
-               NULL, NULL, &da);
-
-  // Set up DMDA
-  DMSetFromOptions(da);
-  DMSetUp(da);
+  petsc_dmda_init(&da);
 
   // Create Petsc vectors for u, v, and p
   Vec u_vec, v_vec, p_vec, u_hat_vec, v_hat_vec;
@@ -69,13 +71,17 @@ int main(int argc, char **argv) {
   DMCreateGlobalVector(da, &u_hat_vec);
   DMCreateGlobalVector(da, &v_hat_vec);
 
+  double temp[NY][NX];
+
   // loop
   for (int iter = 0; iter < MAX_ITER; iter++) {
     setBoundaryConditions(da, u_vec, v_vec, p_vec);
-    updateIntermediateVelocity_fs(da, u_vec, v_vec, p_vec, u_hat_vec,
-                                  v_hat_vec);
-    updatePressureField(da, u_vec, v_vec, p_vec);
-    updateVelocityField(da, u_vec, v_vec, p_vec, u_hat_vec, v_hat_vec);
+    // DMDAVecGetArray(da, u_vec, temp);
+    // printMatrix(temp);
+    // updateIntermediateVelocity_fs(da, u_vec, v_vec, p_vec, u_hat_vec,
+    //                               v_hat_vec);
+    // updatePressureField(da, u_vec, v_vec, p_vec);
+    // updateVelocityField(da, u_vec, v_vec, p_vec, u_hat_vec, v_hat_vec);
   }
 
   // Clean up
@@ -89,16 +95,28 @@ int main(int argc, char **argv) {
   return 0;
 }
 
-PetscErrorCode updateIntermediateVelocity_fs(DM da, Vec u_vec, Vec v_vec,
+void printMatrix(double mtx[NY][NX]) {
+  // 결과 출력
+  int divide = 1;
+  for (int j = NY - 1; j >= 0; j--) {
+    for (int i = 0; i < NX; i++) {
+      if (i % divide == 0 && j % divide == 0) {
+        printf("%.1lf ", mtx[i][j]);
+      }
+    }
+    if (j % divide == 0)
+      puts("");
+  }
+}
+
+void updateIntermediateVelocity_fs(DM da, Vec u_vec, Vec v_vec,
                                              Vec p_vec, Vec u_hat_vec,
                                              Vec v_hat_vec) {
-  PetscErrorCode ierr;
-
+  
+  KSP ksp;
   PetscReal **u, **v, **p, **u_hat, **v_hat;
   DMDALocalInfo info;
   PetscInt i, j, mx, my;
-  PetscReal **conx, **cony, **A1u, **A2u, **conyold;
-  PetscReal **A1v, **A2v, **rhsx, **rhsy;
 
   DMDAVecGetArray(da, u_vec, &u);
   DMDAVecGetArray(da, v_vec, &v);
@@ -111,27 +129,20 @@ PetscErrorCode updateIntermediateVelocity_fs(DM da, Vec u_vec, Vec v_vec,
 
   DMDAGetLocalInfo(da, &info);
 
-  conx = (double **)malloc(NX * sizeof(double *));
-  cony = (double **)malloc(NX * sizeof(double *));
-  A1u = (double **)malloc(NX * sizeof(double *));
-  A2u = (double **)malloc(NX * sizeof(double *));
-  conyold = (double **)malloc(NX * sizeof(double *));
-  A1v = (double **)malloc(NX * sizeof(double *));
-  A2v = (double **)malloc(NX * sizeof(double *));
-  rhsx = (double **)malloc(NX * sizeof(double *));
-  rhsy = (double **)malloc(NX * sizeof(double *));
+  // convection variables
+  double conx[NY][NX];
+  double cony[NY][NX];
 
-  for (int i = 0; i < NX; i++) {
-    conx[i] = (double *)malloc(NY * sizeof(double));
-    cony[i] = (double *)malloc(NY * sizeof(double));
-    A1u[i] = (double *)malloc(NY * sizeof(double));
-    A2u[i] = (double *)malloc(NY * sizeof(double));
-    conyold[i] = (double *)malloc(NY * sizeof(double));
-    A1v[i] = (double *)malloc(NY * sizeof(double));
-    A2v[i] = (double *)malloc(NY * sizeof(double));
-    rhsx[i] = (double *)malloc(NY * sizeof(double));
-    rhsy[i] = (double *)malloc(NY * sizeof(double));
-  }
+  // A variables;
+  double A1u[NY][NX];
+  double A2u[NY][NX];
+
+  double A1v[NY][NX];
+  double A2v[NY][NX];
+
+  // RHS variables with f(conv, diff)
+  double rhsx[NY][NX];
+  double rhsy[NY][NX];
 
   for (j = info.ys; j < info.ys + info.ym; j++) {
     for (i = info.xs; i < info.xs + info.xm; i++) {
@@ -176,26 +187,45 @@ PetscErrorCode updateIntermediateVelocity_fs(DM da, Vec u_vec, Vec v_vec,
     }
   }
 
+  Mat A;
+  Vec x, b;
+  double rhsSolutionX[NY][NX], rhsSolutionY[NY][NX];
+
+  petsc_matrixvector_init(&A, &x, &b, da, A1u, A2u);
+  petsc_ksp_init(&ksp, A);
+  petsc_solve(rhsSolutionX, da, &x, &b, ksp, rhsx);
+
+  petsc_matrixvector_init(&A, &x, &b, da, A1v, A2v);
+  petsc_ksp_init(&ksp, A);
+  petsc_solve(rhsSolutionY, da, &x, &b, ksp, rhsy);
+
+  // double _rhsx[NY][NX];
+  // double _rhsy[NY][NX];
+
+  // petsc_matrixvector_init(&A, &x, &b, A2u);
+  // petsc_ksp_init(&ksp, A);
+  // petsc_solve();
+
+  // petsc_matrixvector_init(&A, &x, &b, A1v);
+  // petsc_ksp_init(&ksp, A);
+  // petsc_solve();
+
+  // petsc_matrixvector_init(&A, &x, &b, A2v);
+  // petsc_ksp_init(&ksp, A);
+  // petsc_solve();
+
   // (1 - A1)(1 - A2)X = RHS (X = u_hat - u)
-  double rhsSolutionX_[NY][NX];
-  getLinearSolutionWithThomas(NY, NX, A1u, rhsx, rhsSolutionX_);
 
-  // (1 - A2)X = X'
-  double rhsSolutionX[NX][NY];
-  getLinearSolutionWithThomas(NX, NY, A2u, rhsSolutionX_, rhsSolutionX);
+  // // (1 - A2)X = X'
 
-  // (1 - A1)(1 - A2)Y = RHS (Y = v_hat - v)
-  double rhsSolutionY_[NX][NY];
-  getLinearSolutionWithThomas(NX, NY, A1v, rhsy, rhsSolutionY_);
+  // // (1 - A1)(1 - A2)Y = RHS (Y = v_hat - v)
 
-  // (1 - A2)Y = Y'
-  double rhsSolutionY[NX][NY];
-  getLinearSolutionWithThomas(NX, NY, A2v, rhsSolutionY_, rhsSolutionY);
+  // // (1 - A2)Y = Y'
 
   for (int i = 1; i < NX - 1; ++i) {
     for (int j = 1; j < NY - 1; ++j) {
-      u_hat[i][j] = u[i][j] + rhsSolutionX[i][j];
-      v_hat[i][j] = v[i][j] + rhsSolutionY[i][j];
+      u_hat[j][i] = u[j][i] + rhsSolutionX[j][i];
+      v_hat[j][i] = v[j][i] + rhsSolutionY[j][i];
     }
   }
 
@@ -206,42 +236,9 @@ PetscErrorCode updateIntermediateVelocity_fs(DM da, Vec u_vec, Vec v_vec,
   DMDAVecRestoreArray(da, u_hat_vec, &u_hat);
   DMDAVecRestoreArray(da, v_hat_vec, &v_hat);
 
-  // Free the allocated memory
-  for (int i = 0; i < NX; i++) {
-    free(conx[i]);
-    free(cony[i]);
-    free(A1u[i]);
-    free(A2u[i]);
-    free(conyold[i]);
-    free(A1v[i]);
-    free(A2v[i]);
-    free(rhsx[i]);
-    free(rhsy[i]);
-    free(rhsSolutionX_[i]);
-    free(rhsSolutionX[i]);
-    free(rhsSolutionY_[i]);
-    free(rhsSolutionY[i]);
-  }
-
-  free(conx);
-  free(cony);
-  free(A1u);
-  free(A2u);
-  free(conyold);
-  free(A1v);
-  free(A2v);
-  free(rhsx);
-  free(rhsy);
-  free(rhsSolutionX_);
-  free(rhsSolutionX);
-  free(rhsSolutionY_);
-  free(rhsSolutionY);
-
-  return ierr;
 }
 
-PetscErrorCode updatePressureField(DM da, Vec u_vec, Vec v_vec, Vec p_vec) {
-  PetscErrorCode ierr;
+void updatePressureField(DM da, Vec u_vec, Vec v_vec, Vec p_vec) {
   PetscInt i, j, Ma, Na, mstart, nstart, mend, nend;
 
   DMDAGetInfo(da, NULL, &Ma, &Na, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
@@ -295,8 +292,6 @@ PetscErrorCode updatePressureField(DM da, Vec u_vec, Vec v_vec, Vec p_vec) {
   // DMDAVecRestoreArray(da, u_vec, &uLocal);
   // DMDAVecRestoreArray(da, v_vec, &vLocal);
   DMDAVecRestoreArray(da, p_vec, &pLocal);
-
-  return ierr;
 }
 
 void updateVelocityField(DM da, Vec u_vec, Vec v_vec, Vec p_vec, Vec u_hat_vec,
@@ -330,62 +325,8 @@ void updateVelocityField(DM da, Vec u_vec, Vec v_vec, Vec p_vec, Vec u_hat_vec,
   DMDAVecRestoreArray(da, v_vec, &v);
 }
 
-void thomasMethod2D(int n, double a[], double b[], double c[], double d[][n],
-                    double x[][n]) {
-  // Forward Elimination
-  for (int i = 1; i < n; i++) {
-    double m = a[i] / b[i - 1];
-    b[i] -= m * c[i - 1];
-    for (int j = 0; j < n; j++) {
-      c[i] -= m * c[i - 1];
-    }
-    for (int j = 0; j < n; j++) {
-      d[i][j] -= m * d[i - 1][j];
-    }
-  }
-
-  // Backward Substitution
-  for (int k = 0; k < n; k++) {
-    x[n - 1][k] = d[n - 1][k] / b[n - 1];
-    for (int i = n - 2; i >= 0; i--) {
-      double sum = d[i][k];
-      for (int j = i + 1; j < n; j++) {
-        sum -= c[i] * x[j][k];
-      }
-      x[i][k] = sum / b[i];
-    }
-  }
-}
-
-void getLinearSolutionWithThomas(int dim, int numOfSolutions,
-                                 double A[dim][dim],
-                                 double RHS[][numOfSolutions],
-                                 double x[][numOfSolutions]) {
-  double *a = (double *)malloc(dim * sizeof(double *));
-  double *b = (double *)malloc(dim * sizeof(double *));
-  double *c = (double *)malloc(dim * sizeof(double *));
-
-  for (int i = 0; i < dim; ++i) {
-    for (int j = 0; j < dim; ++j) {
-      if (i - j == 1)
-        a[i] = -x[i][j];
-      else if (i == j)
-        b[i] = 1 - x[i][j];
-      else if (i - j == -1)
-        c[i] = -x[i][j];
-    }
-  }
-  a[0] = 0.0;
-  c[dim - 1] = 0.0;
-  thomasMethod2D(numOfSolutions, a, b, c, RHS, x);
-  free(a);
-  free(b);
-  free(c);
-}
-
 // 경계 조건 설정 함수
 void setPressureBoundaryConditions(DM da, Vec p_vec) {
-  PetscErrorCode ierr;
   DMDALocalInfo info;
 
   DMDAGetLocalInfo(da, &info);
@@ -431,31 +372,31 @@ void setVelocityBoundaryConditions(DM da, Vec u_vec, Vec v_vec) {
   // Set velocity boundary conditions
   for (PetscInt i = info.xs; i < info.xs + info.xm; i++) {
     // Top boundary (lid-driven): u=U_WALL, v=0
-    gamma_top = (vArray[i][info.ys + info.ym - 2] - yc[NY - 2]) /
+    gamma_top = (vArray[info.ys + info.ym - 2][i] - yc[NY - 2]) /
                 (yc[NY - 2] - yc[NY - 3]);
-    uArray[i][info.ys + info.ym - 1] =
-        (U_WALL - (1 - gamma_top) * uArray[i][info.ys + info.ym - 2]) /
+    uArray[info.ys + info.ym - 1][i] =
+        (U_WALL - (1 - gamma_top) * uArray[info.ys + info.ym - 2][i]) /
         gamma_top;
-    vArray[i][info.ys + info.ym - 2] = 0.0;
+    vArray[info.ys + info.ym - 2][i] = 0.0;
     // Bottom boundary: u=0, v=0
-    gamma_bottom = (vArray[i][info.ys] - yc[0]) / (yc[1] - yc[0]);
-    uArray[i][info.ys] =
-        -gamma_bottom / (1 - gamma_bottom) * uArray[i][info.ys + 1];
-    vArray[i][info.ys] = 0.0;
+    gamma_bottom = (vArray[info.ys][i] - yc[0]) / (yc[1] - yc[0]);
+    uArray[info.ys] =
+        -gamma_bottom / (1 - gamma_bottom) * uArray[info.ys + 1][i];
+    vArray[info.ys][i] = 0.0;
   }
 
   for (PetscInt j = info.ys; j < info.ys + info.ym; j++) {
     // Left boundary: u=0, v=0
-    gamma_west = (uArray[info.xs][j] - xc[0]) / (xc[1] - xc[0]);
-    uArray[info.xs][j] = 0.0;
-    vArray[info.xs][j] =
-        -gamma_west / (1 - gamma_west) * vArray[info.xs + 1][j];
+    gamma_west = (uArray[j][info.xs] - xc[0]) / (xc[1] - xc[0]);
+    uArray[j][info.xs] = 0.0;
+    vArray[j][info.xs] =
+        -gamma_west / (1 - gamma_west) * vArray[j][info.xs + 1];
     // Right boundary: u=0, v=0
-    gamma_east = (uArray[info.xs + info.xm - 2][j] - xc[NX - 2]) /
+    gamma_east = (uArray[j][info.xs + info.xm - 2] - xc[NX - 2]) /
                  (xc[NX - 2] - xc[NX - 3]);
-    uArray[info.xs + info.xm - 2][j] = 0.0;
-    vArray[info.xs + info.xm - 1][j] =
-        -(1 - gamma_east) / gamma_east * vArray[info.xs + info.xm - 2][j];
+    uArray[j][info.xs + info.xm - 2] = 0.0;
+    vArray[j][info.xs + info.xm - 1] =
+        -(1 - gamma_east) / gamma_east * vArray[j][info.xs + info.xm - 2];
   }
 
   // Restore arrays
@@ -466,4 +407,221 @@ void setVelocityBoundaryConditions(DM da, Vec u_vec, Vec v_vec) {
 void setBoundaryConditions(DM da, Vec u_vec, Vec v_vec, Vec p_vec) {
   setPressureBoundaryConditions(da, p_vec);
   setVelocityBoundaryConditions(da, u_vec, v_vec);
+}
+
+
+PetscErrorCode petsc_matrixvector_init(Mat *A, Vec *x, Vec *b, DM da, double A1_fs[][NX], double A2_fs[][NX])
+{
+    // create matrix, vector
+    PetscErrorCode ierr;
+
+    MatStencil matrow, matcol[7];
+    PetscScalar matv[7];
+    PetscInt xs, ys, zs, xm, ym, zm;
+
+    ierr = DMDAGetCorners(da, &xs, &ys, &zs, &xm, &ym, &zm);
+    ierr = DMSetMatType(da, "aij");
+    ierr = DMCreateMatrix(da, A);
+
+    // if(SHIFT_FLAG==0)
+    int i, j;
+    {
+        // global index system
+            for (j = ys; j < ys + ym; j++)
+            {
+                for (i = xs; i < xs + xm; i++)
+                {
+                    matrow.i = i;
+                    matrow.j = j;
+
+                    int cnt = 0;
+
+                    if (i == j) {
+                      matv[cnt] = 1 - A1_fs[j][i] - A2_fs[j][i];
+                      matcol[cnt].i = i;
+                      matcol[cnt].j = j;
+                      cnt++;
+                    } else if (abs(i - j) == 1) {
+                      matv[cnt] = -A1_fs[j][i] - A2_fs[j][i];
+                      matcol[cnt].i = i;
+                      matcol[cnt].j = j;
+                      cnt++;
+                    }
+
+                    ierr = MatSetValuesStencil(*A, 1, &matrow, cnt, matcol, matv, INSERT_VALUES);
+                    // CHKERRQ(ierr);
+                }
+            }
+        
+    }
+
+    MatAssemblyBegin(*A, MAT_FINAL_ASSEMBLY);
+    MatAssemblyEnd(*A, MAT_FINAL_ASSEMBLY);
+
+    ierr = DMCreateGlobalVector(da, b);
+    ierr = DMCreateGlobalVector(da, x);
+    CHKERRQ(ierr);
+    return ierr;
+}
+
+PetscErrorCode petsc_ksp_init(KSP *ksp, Mat A)
+{
+    PetscErrorCode ierr;
+    PetscReal tol_rel = 0.0001;
+
+    ierr = KSPCreate(PETSC_COMM_WORLD, ksp);
+    ierr = KSPSetOperators(*ksp, A, A);
+
+    ierr = PCCreate(PETSC_COMM_WORLD, &pc);
+
+    // ********** original ************
+    // ierr = KSPSetType(*ksp, KSPCG);
+    // ierr = KSPSetTolerances(*ksp, tol_rel, PETSC_DEFAULT, PETSC_DEFAULT, max_iter);
+    // ierr = KSPSetFromOptions(*ksp);
+    // ierr = KSPSetUp(*ksp);
+
+    // ********** LU ************
+    // ierr = KSPGetPC(*ksp, &pc);
+    // ierr = PCSetType(pc , PCLU);
+    // ierr = PCFactorSetMatSolverType(pc, MATSOLVERSUPERLU_DIST);
+
+    // ierr = KSPSetType(*ksp, KSPCG);
+    // ierr = KSPSetTolerances(*ksp, tol_rel, PETSC_DEFAULT, PETSC_DEFAULT, max_iter);
+    // ierr = KSPSetFromOptions(*ksp);
+    // ierr = KSPSetUp(*ksp);
+
+    // ********** AMG ************
+    ierr = KSPSetType(*ksp, KSPPREONLY);
+    ierr = KSPGetPC(*ksp, &pc);
+    ierr = PCSetType(pc, PCGAMG);
+    ierr = PCMGSetLevels(pc, 2, NULL);
+
+    ierr = KSPSetTolerances(*ksp, tol_rel, PETSC_DEFAULT, PETSC_DEFAULT, MAX_ITER);
+    ierr = KSPSetFromOptions(*ksp);
+    ierr = KSPSetUp(*ksp);
+    CHKERRQ(ierr);
+    return ierr;
+}
+
+PetscErrorCode petsc_solve(double solution[][NX], DM da, Vec *x, Vec *b, KSP ksp, double rhs[][NX])
+{
+    PetscErrorCode ierr;
+    PetscScalar **xx, **bb;
+
+    PetscInt xs, ys, zs, xm, ym, zm;
+    ierr = DMDAGetCorners(da, &xs, &ys, &zs, &xm, &ym, &zm);
+
+    ierr = DMDAVecGetArray(da, *b, &bb);
+    // if(myid==0) printf("check_petsc\n");
+
+    int i, j;
+    for (j = 1; j < NY - 1; j++)
+    {
+        for (i = 1; i < NX - 1; i++)
+        {
+
+            bb[j - 1 + ys][i - 1 + xs] = rhs[j][i];
+        }
+    }
+    // if(myid==0) printf("check_petsc\n");
+
+    ierr = DMDAVecRestoreArray(da, *b, &bb);
+
+    // if(myid==0) printf("check_petsc\n");
+
+    VecAssemblyBegin(*b);
+    VecAssemblyEnd(*b);
+
+    // if(myid==0) printf("check_petsc\n");
+
+    ierr = DMDAVecGetArray(da, *x, &xx);
+        for (j = 1; j < NY - 1; j++)
+        {
+            for (i = 1; i < NX - 1; i++)
+            {
+                xx[j - 1 + ys][i - 1 + xs] = solution[i][j];
+            }
+        }
+
+    ierr = DMDAVecRestoreArray(da, *x, &xx);
+    ierr = VecAssemblyBegin(*x);
+    ierr = VecAssemblyEnd(*x);
+
+    // if(myid==0) printf("check_petsc\n");
+
+    PetscInt its;
+    PetscReal rnorm;
+
+    ierr = KSPSetFromOptions(ksp);
+    ierr = KSPSolve(ksp, *b, *x);
+    ierr = KSPGetIterationNumber(ksp, &its);
+    ierr = KSPGetResidualNorm(ksp, &rnorm);
+    ierr = KSPGetSolution(ksp, x);
+
+   // if (myid == 0)
+     //   printf("petsc finish! %d/%e \n", its, rnorm);
+
+    ierr = DMDAVecGetArray(da, *x, &xx); // From vector to array
+        for (j = 1; j < NY - 1; j++)
+        {
+            for (i = 1; i < NX - 1; i++)
+            {
+                solution[i][j] = xx[j - 1 + ys][i - 1 + xs];
+            }
+        }
+    ierr = DMDAVecRestoreArray(da, *x, &xx);
+    CHKERRQ(ierr); 
+    return ierr;
+}
+
+
+void petsc_dmda_init(DM *da)
+{
+    // create dmda
+    PetscErrorCode ierr;
+
+    PetscInt *lx;
+    PetscInt *ly;
+
+    lx = (PetscInt *)calloc(MPIx, sizeof(PetscInt));
+    ly = (PetscInt *)calloc(MPIy, sizeof(PetscInt));
+
+    int i;
+
+    for (i = 0; i < MPIx; i++)
+    {
+        int tempsize;
+        tempsize = (int)round((double)nx / MPIx);
+        // if (i==(MPIx-1)) lx[i]=nx-(MPIx-1)*tempsize-1;
+        if (i == (MPIx - 1))
+            lx[i] = nx - (MPIx - 1) * tempsize;
+        // else if (i==0) lx[i] = tempsize-1;
+        else
+            lx[i] = tempsize;
+    }
+
+    for (i = 0; i < MPIy; i++)
+    {
+        int tempsize;
+        tempsize = (int)round((double)ny / MPIy);
+        // if (i==(MPIy-1)) ly[i]=ny-(MPIy-1)*tempsize-1;
+        if (i == (MPIy - 1))
+            ly[i] = ny - (MPIy - 1) * tempsize;
+        // else if (i==0) ly[i] = tempsize-1;
+        else
+            ly[i] = tempsize;
+    }
+
+    PetscInt dof = 1;
+    PetscInt stencil_width = 1;
+
+    ierr = DMDACreate2d(PETSC_COMM_WORLD, DM_BOUNDARY_NONE, DM_BOUNDARY_NONE,
+                        DMDA_STENCIL_STAR, nx, ny, MPIx, MPIy, dof, stencil_width, lx, ly, da);
+    // DMDA_STENCIL_STAR, nx-2, ny-2, nz-2, MPIx, MPIy, MPIz, dof, stencil_width, lx, ly, lz, da);
+
+    ierr = DMSetFromOptions(*da);
+    ierr = DMSetUp(*da);
+
+    free(lx);
+    free(ly);
 }
